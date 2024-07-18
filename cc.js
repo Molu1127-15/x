6cb4542553dc5d8adbcab993215b3fa4ba741d8a
Molu1127-15 <83622425+Molu1127-15@users.noreply.github.com>
2024-07-19 00:58:58 +0800

    Update cc.js

--- cc.js
+++ cc.js
@@ -23,7 +23,7 @@
 # 去广告
 ^https?:\/\/qqcapi.*.com\/api\/(promo\/popup|task_ad\/list|operation\/ads|investment|ad\/ad_position_config) url reject-200
 # 重写
-^https?:\/\/qqcapi.*.com\/api\/(v2\/bootstrap|module\/list|user\/read|bottom_nav|navs\/top|rich_new\/index|user\/privilege|v3\/shortvideo\/view\/\d+) url script-response-body https://raw.githubusercontent.com/Molu1127-15/x/blob/main/cc.js
+^https?:\/\/qqcapi.*.com\/api\/(v2\/bootstrap|module\/list|user\/read|bottom_nav|navs\/top|rich_new\/index|user\/privilege|v3\/shortvideo\/view\/\d+) url script-response-body https://raw.githubusercontent.com/Molu1127-15/x/main/cc.js
 hostname = qqcapi.*.com
 
 ******************************************/
